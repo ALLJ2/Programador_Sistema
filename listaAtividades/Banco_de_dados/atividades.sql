f30198e6f6b473a6634b58e747e6300e8ddb8afc
ALEXSANDRO LUCIANO DE JESUS <alexsandro.ljesus@senacsp.edu.br>
2025-04-11 22:27:08 -0300

    aaaaa

---
+++ listaAtividades/Banco_de_dados/atividades.sql
@@ -0,0 +1,27 @@
+CREATE TABLE atividade (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    situacao INT NOT NULL DEFAULT 0
+);
+
+insert into atividade (titulo)
+values (@titulo);
+
+UPDATE atividade 
+SET 
+    situacao = @situacao
+WHERE
+    id = @id;
+
+SELECT 
+    *
+FROM
+    atividade
+WHERE
+    situacao = 0;
+SELECT 
+    *
+FROM
+    atividade
+WHERE
+    situacao = 1;
